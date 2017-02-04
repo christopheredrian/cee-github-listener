2e1bf0801b03cb2eccbac42b7803c75cb6f0c937
Christopher Espiritu <christopheredrian@gmail.com>
2017-02-04 23:02:34 +0800

    added quickstart files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+app.set('port', (process.env.PORT || 80));
 var cred = JSON.parse(fs.readFileSync('./scrt.json', "utf-8"));
 
 // login({email: "cee.bot.7", password: cred.key}, function callback (err, api) {
@@ -14,18 +14,22 @@ var cred = JSON.parse(fs.readFileSync('./scrt.json', "utf-8"));
 //     api.sendMessage(msg, yourID);
 // });
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // POST method route
 app.post('/ghpush', function (req, res) {
-//   res.send('POST request to the homepage')
+    //   res.send('POST request to the homepage')
     console.log(JSON.stringify(req.body));
     res.send("body: " + JSON.stringify(req.body));
 });
 
-app.get('/ghpush', function(req, res){
+app.get('/ghpush', function (req, res) {
     res.send("bd: " + JSON.stringify(req.body));
 });
 
 
-app.listen(80);
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
