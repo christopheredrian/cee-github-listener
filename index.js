e1725a72d95626e011a5a034fcfbb73e23068f73
Christopher Espiritu <christopheredrian@gmail.com>
2017-02-04 23:31:09 +0800

    the body

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.post('/ghpush', function (req, res) {
         if (err) return console.error(err);
         // var yourID = 100000080590639;
         var yourID = 1323709537696090
-        var bodyMsg = "Response of git: " + JSON.stringify(req.body) + "Compare: " + req.body.compare;
+        // var bodyMsg = "Response of git: " + JSON.stringify(req.body) + "Compare: " + req.body.compare;
+        var bodyMsg = "this is the body";
         var msg = {
             body: bodyMsg
         };
