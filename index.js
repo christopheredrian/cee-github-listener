2c0defe2a7f78596ae2b9d12fba3b67822e61aa6
Christopher Espiritu <christopheredrian@gmail.com>
2017-02-05 10:19:27 +0800

    Beautify

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-var login = require("facebook-chat-api");
-var fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.set('port', (process.env.PORT || 80));
-
-var cred = JSON.parse(fs.readFileSync('./scrt.json', "utf-8"));
+var fs = require('fs');
+var login = require("facebook-chat-api");
+//  Middlewares
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+var cred = JSON.parse(fs.readFileSync('./scrt.json', "utf-8"));
+app.set('port', (process.env.PORT || 80));
+
+
 
 // POST method route
 app.post('/ghpush', function (req, res) {
-    //   res.send('POST request to the homepage')
-    var json = req.body;
     login({
         email: "cee.bot.7",
         password: cred.key
     }, function callback(err, api) {
         if (err) return console.error(err);
         var yourID = 1323709537696090
-        var bodyMsg = "";
-        if(json.repository.name){
-            bodyMsg += "\nRepo name: " + json.repository.name;
-        } else{
-            bodyMsg += "No repository name";
-        }
+        var body = req.body;
         var msg = {
             body: bodyMsg
         };
@@ -34,60 +29,12 @@ app.post('/ghpush', function (req, res) {
         res.sendStatus(200);
     });
 });
-
+// GET method route
 app.get('/ghpush', function (req, res) {
     res.send("body: " + JSON.stringify(req.body));
 
 });
 
-
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
-
-// wh server below
-// var http = require('http')
-// var createHandler = require('github-webhook-handler')
-// var handler = createHandler({
-//     path: '/https://cee-github-listener.herokuapp.com/ghpush', secret: ''
-// })
-
-// http.createServer(function (req, res) {
-//     handler(req, res, function (err) {
-//         res.statusCode = 404
-//         res.end('no such location')
-//     })
-// }).listen(process.env.PORT || 80)
-
-// handler.on('error', function (err) {
-//     console.error('Error:', err.message)
-// })
-
-// handler.on('push', function (event) {
-//     console.log('Received a push event for %s to %s',
-//         event.payload.repository.name,
-//         event.payload.ref)
-//     login({
-//         email: "cee.bot.7",
-//         password: cred.key
-//     }, function callback(err, api) {
-//         if (err) return console.error(err);
-//         // var yourID = 100000080590639;
-//         var yourID = 1323709537696090
-//         var bodyMsg = "Response of git: " + event.payload.repository.name;
-//         var msg = {
-//             body: bodyMsg
-//         };
-//         api.sendMessage(msg, yourID);
-//         // res.send("on push: " + JSON.stringify(req.body));
-//         res.sendStatus(200);
-//     });
-// })
-
-// handler.on('issues', function (event) {
-//     console.log('Received an issue event for %s action=%s: #%d %s',
-//         event.payload.repository.name,
-//         event.payload.action,
-//         event.payload.issue.number,
-//         event.payload.issue.title)
-// })
